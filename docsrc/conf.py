cc3f592abcbdd46079951be749e1ed9a3661c5e3
Cary Coutant <ccoutant@gmail.com>
2024-03-29 17:41:58 -0700

    Turn off links on images.

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -49,6 +49,7 @@ html_theme_options = {
     'font_family': '"Open Sans", sans-serif',
 }
 html_copy_source = False;
+html_scaled_image_link = False;
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
